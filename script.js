897ea3c000d691cd4e299de1aba8b28890107460
Jacekzaw1994 <Jacekzaw1994@gmail.com>
2017-04-16 21:21:12 +0200

    Key handling

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.addEventListener("keypress", function (e) {
-    console.log(e.keyCode);
+window.addEventListener("keypress", function (e) {
+    const keyCode = e.keyCode;
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
 });
+
+
+function removeTransform(e){
+    if (e.propertyName !== 'transform') return; // skip if it is not a transform
+    this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransform));
