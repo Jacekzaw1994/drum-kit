afda9ac2a4e5894979311f742602defba732b167
Jacekzaw1994 <Jacekzaw1994@gmail.com>
2017-04-17 14:05:12 +0200

    Music bars animation

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-window.addEventListener("keypress", function (e) {
-    const keyCode = e.keyCode;
+function playSounds(e) {
+    const keyCode = e.keyCode < 90 ? e.keyCode : e.keyCode - 32; //
     const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${keyCode}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
+    const bar = document.querySelector(`.bar[data-key="${keyCode}"]`);
+    if (!audio) return; //if audio isn't find, stop the fuction
+    audio.currentTime = 0; // rewind to the start
     audio.play();
     key.classList.add('playing');
-});
-
+    bar.classList.add('active');
+};
 
 function removeTransform(e){
     if (e.propertyName !== 'transform') return; // skip if it is not a transform
     this.classList.remove('playing');
+    const bar = document.querySelector(`.bar[data-key="${this.getAttribute("data-key")}"]`); //find the bar with the same data-key
+    bar.classList.remove('active');
 };
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend',removeTransform));
+
+window.addEventListener("keypress", playSounds);
